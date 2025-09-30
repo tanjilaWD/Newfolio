b4ca809929298b0f71784f6d8c79e3cb78309fce
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-09-30 20:57:53 +0600

    new

--- src/Components/UI/Hero.jsx
+++ src/Components/UI/Hero.jsx
@@ -3,7 +3,7 @@ const Hero = () => {
         <section className="container">
             <div className="hero">
                 <div className="hero_left">
-                    <h1>👋 Hey, I’m Tushar Full-Stack Engineer.</h1>
+                    <h1>👋 Hey, I’m Tanjila <br /> Full-Stack Engineer.</h1>
                     <p>Full Stack Frontend Developer and Content Writing Based on LA. I specialize in Bug Fixing, Responsive Web Design, and Visual Development.</p>
                     <div className="btn">
                         <button>Hire me <img src="../../public/image/hero_icon/vector.png" alt="icon" /></button>
