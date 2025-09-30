27cce495e109bef30c55c3c0f3b06277f5a7c725
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-09-30 18:28:17 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./Components/UI/Header";
+
+
+const App = () => {
+    return (
+        <div>
+            <Header/>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/Components/UI/Header.jsx
@@ -0,0 +1,29 @@
+
+
+const Header = () => {
+    return (
+       <section className="container">
+            <div className="header">
+                <div className="header_left">
+                    <img src="../../public/image/logo/photo.png" alt="" />
+                    <h2>Newfolio</h2>
+                </div>
+                <div className="header_right">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Project</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Resume</a></li>
+                        <li><a href="#">Testimonial</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Article</a></li>
+                    </ul>
+
+                </div>
+            </div>
+
+        </section>
+    );
+};
+
+export default Header;
