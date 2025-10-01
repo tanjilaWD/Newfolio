6633b8618a8479197c2d29c0051b4329a2606108
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-01 12:57:28 +0600

    new

--- src/Components/UI/Service.jsx
+++ src/Components/UI/Service.jsx
@@ -1,10 +1,36 @@
+import Service_Card from "./Service_Card";
 
 
 const Service = () => {
     return (
-       <section className="service_section">
-        
+        <section className="container">
+       <div className="service_section">
+        <div className="text_section">
+            <h2>My Services</h2>
+            <p>Morbi risus elit, fringilla in cursus a, molestie non dui. Nunc accumsan gravida risus, ac semper libero mollis ut.</p>
+        </div>
+        <div className="img_section">
+           <Service_Card
+           image="../../public/image/service/service1.png"
+           title="Back-end Development"
+           description="Vivamus in quam ut felis consectetur imperdiet et sed metus. Nullam tristique scelerisque metus id dignissim."
+           />
 
+           <Service_Card
+           image="../../public/image/service/service2.png"
+           title="Front-end Development"
+           description="Vivamus in quam ut felis consectetur imperdiet et sed metus. Nullam tristique scelerisque metus id dignissim."
+           />
+
+           <Service_Card
+           image="../../public/image/service/service3.png"
+           title="Content Writing"
+           description="Vivamus in quam ut felis consectetur imperdiet et sed metus. Nullam tristique scelerisque metus id dignissim."
+           />
+
+        </div>
+
+       </div>
        </section>
     );
 };
---
+++ src/Components/UI/Service_Card.jsx
@@ -0,0 +1,17 @@
+
+
+const Service_Card = ({image, title, description}) => {
+    return (
+        <div className="service_card">
+            <img src={image} alt="" />
+            <div className="service_card_description">
+                <h3>{title}</h3>
+                <p>{description}</p>
+                <a href="#">Let’s Work Together <img src="../../public/image/service/ArrowRight.png" alt="" /></a>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Service_Card;
