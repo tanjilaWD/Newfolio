59a421d655138bd2972374ab3906c9accd440750
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-01 13:21:44 +0600

    new

--- src/Components/UI/Service.jsx
+++ src/Components/UI/Service.jsx
@@ -3,15 +3,15 @@ import Service_Card from "./Service_Card";
 
 const Service = () => {
     return (
-        <section className="container">
+      
        <div className="service_section">
         <div className="text_section">
             <h2>My Services</h2>
-            <p>Morbi risus elit, fringilla in cursus a, molestie non dui. Nunc accumsan gravida risus, ac semper libero mollis ut.</p>
+            <p>Morbi risus elit, fringilla in cursus a, molestie non dui. <br /> Nunc accumsan gravida risus, ac semper libero mollis ut.</p>
         </div>
         <div className="img_section">
            <Service_Card
-           image="../../public/image/service/service1.png"
+           image="../../public/image/service/service3.png"
            title="Back-end Development"
            description="Vivamus in quam ut felis consectetur imperdiet et sed metus. Nullam tristique scelerisque metus id dignissim."
            />
@@ -23,7 +23,7 @@ const Service = () => {
            />
 
            <Service_Card
-           image="../../public/image/service/service3.png"
+           image="../../public/image/service/service1.png"
            title="Content Writing"
            description="Vivamus in quam ut felis consectetur imperdiet et sed metus. Nullam tristique scelerisque metus id dignissim."
            />
@@ -31,7 +31,7 @@ const Service = () => {
         </div>
 
        </div>
-       </section>
+       
     );
 };
 
