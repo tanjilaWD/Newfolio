fb0b1467870c112647614a6ef279f97b1fe58e13
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-01 11:07:48 +0600

    new

--- src/Components/UI/CaseStudy.jsx
+++ src/Components/UI/CaseStudy.jsx
@@ -3,9 +3,18 @@
 const CaseStudy = () => {
     return (
        <section className="container">
-        <div className="caseStuday">
+        <div className="caseStudy">
+            <div className="caseContent">
+            <div className="content">
             <h2>Latest Case Study</h2>
             <p>Morbi risus elit, fringilla in cursus a, molestie non dui. Nunc accumsan gravida risus, ac semper libero mollis ut.</p>
+            </div>
+            <div className="icon">
+                <img src="../../public/image/case/ArrowRight.png" alt="icon" />
+                <img src="../../public/image/case/ArrowRight1.png" alt="icon" />
+
+            </div>
+            </div>
 
             <div className="caseStudy_list">
                 <img
