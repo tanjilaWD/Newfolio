d8c03ac96a4e7ac3a371742b4679ee798ece4f80
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-01 19:33:26 +0600

    new

--- src/Components/UI/Achievement.jsx
+++ src/Components/UI/Achievement.jsx
@@ -5,26 +5,34 @@ const Achievement = () => {
        <section className="container">
           <div className="achievement_section">
             <div className="record">
+               <div className="number">
                 <Achievement_card
                 image="../../public/image/achievement/Stack.png"
                 title="23+"
                 description="Years of experience"
                 />
+                </div>
+                <div className="number">
                 <Achievement_card
                 image="../../public/image/achievement/Briefcase.png"
                 title="325+"
                 description="Completed Projects"
                 />
+                </div>
+                <div className="number">
                 <Achievement_card
                 image="../../public/image/achievement/SmileyWink.png"
                 title="279+"
                 description="Happy Clients"
                 />
+                </div>
+                <div className="number">
                 <Achievement_card
                 image="../../public/image/achievement/Coffee.png"
                 title="1652+"
                 description="Hours of Work"
                 />
+                </div>
             </div>
           </div>
        </section>
---
+++ src/Components/UI/Testimonial.jsx
@@ -0,0 +1,19 @@
+
+
+const Testimonial = () => {
+    return (
+        <section className="testimonial_section">
+            <div className="testimonial">
+                <div className="testimonial_left">
+
+                </div>
+                <div className="testimonial_right">
+                    
+                </div>
+            </div>
+
+        </section>
+    );
+};
+
+export default Testimonial;
