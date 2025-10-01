1d3b7cee013e89ffaf017067b6c240e8a9f35ffc
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-01 23:56:26 +0600

    new

--- src/Components/UI/Footer.jsx
+++ src/Components/UI/Footer.jsx
@@ -16,10 +16,30 @@ const Footer = () => {
 
             </div>
             <div className="footer_right">
+                
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Resume</a></li>
+               <li><a href="#">Article</a></li>
+            </ul>
 
-            </div>
-          </div>
-        </section>
+           <ul>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a className="active" href="#">Pricing Plans</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+
+         <ul>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">CodePen</a></li>
+         </ul>
+       </div>
+      </div>
+    </section>
     );
 };
 
