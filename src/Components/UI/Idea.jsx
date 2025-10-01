374025595fd0a404b80a5f80e6cbf4b2a64f70d6
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-01 22:01:42 +0600

    new

--- src/Components/UI/Idea.jsx
+++ src/Components/UI/Idea.jsx
@@ -9,18 +9,29 @@ const Idea = () => {
                 <p>Do you fancy saying hi to me or do you want to get started with your project and you need my help? Feel free to contact me.</p>
 
             <div className="idea_btn">
-                <a href="#">let’ work together <img src="../../public/image/idea/ArrowRight.png" alt="image" /></a>
+                <a className="BtnIdea" href="#">let’ work together <img src="../../public/image/idea/ArrowRight.png" alt="image" /></a>
             </div>
 
             </div>
             <div className="idea_right">
                 <div className="mail">
-
+                    <div className="image">
+                       <img src="../../public/image/idea/Envelope.png" alt="Image" />
+                    </div>
+                    <div className="content">
+                        <p>Chat with me</p>
+                        <h4>tanjilabithi12@gmail.com</h4>
+                    </div>
+                    </div>
+                    <div className="call">
+                    <div className="image">
+                        <img src="../../public/image/idea/PhoneCall.png" alt="Image" />
+                    </div>
+                    <div className="content">
+                        <p>Call Me</p>
+                        <h4>+1-202-555-0190</h4>
+                    </div>
                 </div>
-                <div className="call">
-                    
-                </div>
-
             </div>
         </div>
       </section>
