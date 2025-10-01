d99e5dabadb30e081fe1b2106559b9af75fd38ee
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-01 18:37:33 +0600

    new

--- src/Components/UI/Service.jsx
+++ src/Components/UI/Service.jsx
@@ -10,23 +10,27 @@ const Service = () => {
             <p>Morbi risus elit, fringilla in cursus a, molestie non dui. <br /> Nunc accumsan gravida risus, ac semper libero mollis ut.</p>
         </div>
         <div className="img_section">
+            <div className="card">
            <Service_Card
            image="../../public/image/service/service3.png"
            title="Back-end Development"
            description="Vivamus in quam ut felis consectetur imperdiet et sed metus. Nullam tristique scelerisque metus id dignissim."
            />
-
+           </div>
+            <div className="card">
            <Service_Card
            image="../../public/image/service/service2.png"
            title="Front-end Development"
            description="Vivamus in quam ut felis consectetur imperdiet et sed metus. Nullam tristique scelerisque metus id dignissim."
            />
-
+           </div>
+           <div className="card">
            <Service_Card
            image="../../public/image/service/service1.png"
            title="Content Writing"
            description="Vivamus in quam ut felis consectetur imperdiet et sed metus. Nullam tristique scelerisque metus id dignissim."
            />
+           </div>
 
         </div>
 
--- src/Components/UI/Service_Card.jsx
+++ src/Components/UI/Service_Card.jsx
@@ -3,11 +3,11 @@
 const Service_Card = ({image, title, description}) => {
     return (
         <div className="service_card">
-            <img src={image} alt="" />
+            <img className="abc" src={image} alt="" />
             <div className="service_card_description">
                 <h3>{title}</h3>
                 <p>{description}</p>
-                <a href="#">Let’s Work Together <img src="../../public/image/service/ArrowRight.png" alt="" /></a>
+                <a href="#">Let’s Work Together <img className="arrow" src="../../public/image/service/ArrowRight.png" alt="" /></a>
             </div>
             
         </div>
